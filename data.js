f20f3ea045012fb78f4b442b2f27ab7cf4534c57
BO55130SS <gundam.d.130ss@gmail.com>
2026-07-03 11:08:40 +0900

    Update data.js

--- data.js
+++ data.js
@@ -1,7 +1,9 @@
 const factionData = {
   "Space Marines": {
     name: "Space Marines",
-    detachments: ["Gladius Task Force"],
+    detachments: [
+      "Gladius Task Force"
+    ],
     armyRules: [
       {
         name: "Oath of Moment",
@@ -13,7 +15,9 @@ const factionData = {
 
   "Orks": {
     name: "Orks",
-    detachments: ["War Horde"],
+    detachments: [
+      "War Horde"
+    ],
     armyRules: [
       {
         name: "Waaagh!",
