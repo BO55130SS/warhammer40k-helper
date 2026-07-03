a0363dd2a4c93134d32856719908be618ec31fe4
BO55130SS <gundam.d.130ss@gmail.com>
2026-07-03 10:49:07 +0900

    Create data.js

---
+++ data.js
@@ -0,0 +1,29 @@
+const factionData = {
+  "Space Marines": {
+    name: "Space Marines",
+    detachments: [
+      "Gladius Task Force"
+    ],
+    armyRules: [
+      {
+        name: "Oath of Moment",
+        timing: "Command Phase",
+        summary: "自軍コマンドフェイズで敵ユニット1つを選ぶ。"
+      }
+    ]
+  },
+
+  "Orks": {
+    name: "Orks",
+    detachments: [
+      "War Horde"
+    ],
+    armyRules: [
+      {
+        name: "Waaagh!",
+        timing: "Command Phase",
+        summary: "1ゲームに1回、オルク全軍を強化する。"
+      }
+    ]
+  }
+};
